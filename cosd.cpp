af5dc23fddaf53ecfd816e796b1ec8b7fa231039
gitmah01720 <maheshpandeyindia@gmail.com>
2023-03-01 22:56:41 +0530

    applied all changes

---
+++ cosd.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef vector<int> vi;
+typedef  long int li;
+typedef  long long int lli;
+typedef unsigned long long int ulli;
+int main()
+{
+ string ss;
+ ss="aklsdjhhfg"; // made some changes
+ cout<<ss<<'\n';
+ 
+//  ldkjfhgldkjfhg
+ // adding some changes in file commitn tidflkgjdfg dfgdjkfgakjhfkajdg adf giadhjgoierkgjoidfujdfa .,
+ 
+ int x;
+ 
+}
